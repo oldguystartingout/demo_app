183623d2313399bdf108ca0c322a17ce25ea3aca
Lloyd <lloyd@bunting.com.au>
2011-11-18 18:56:22 +1100

    Inital commit of new app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DemoApp::Application.config.secret_token = 'd0ac5e8ca6e9d7a4f39f6755557d26d682a9662eb03b6a4985fd59231fe2a4036138f213db2e0d32c826f82a8128b896fe84bb94c72f10befa86910e9c07eed6'
